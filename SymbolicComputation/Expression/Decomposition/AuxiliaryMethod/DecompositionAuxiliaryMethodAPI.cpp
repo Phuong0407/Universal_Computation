7189ac7c3465df9926f1a75bd691398cfabc8882
Phuong0407 <diepthanhphuong0407@gmail.com>
2022-07-03 20:01:50 +0700

    Create decomposition auxiliary method API

---
+++ SymbolicComputation/Expression/Decomposition/AuxiliaryMethod/DecompositionAuxiliaryMethodAPI.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file DecompositionAuxiliaryMethodAPI.cpp
+ * @author Phuong Diep Thanh (diepthanhphuong0407@gmail.com)
+ * @version 2.0
+ * @date 2022-07-03
+ * @copyright Copyright (c) 2022
+ */
+#ifndef DECOMPOSITION_AUXILIARY_METHOD_API_CPP
+#define DECOMPOSITION_AUXILIARY_METHOD_API_CPP
+
+#include "IsDigit.cpp"
+#include "IsOperand.cpp"
+#include "IsBalance.cpp"
+#include "IsOperator.cpp"
+#include "IsConstant.cpp"
+#include "IsBuiltInFunction.cpp"
+#include "GetInsideParentheses.cpp"
+#include "PriorityOrderCompute.cpp"
+
+#endif
