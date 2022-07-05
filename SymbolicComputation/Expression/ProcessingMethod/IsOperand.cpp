998c2ba40834cfa69839147fcc26b7da27f1062f
Phuong0407 <diepthanhphuong0407@gmail.com>
2022-07-05 18:06:10 +0700

    Test, OK!

---
+++ SymbolicComputation/Expression/ProcessingMethod/IsOperand.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file IsOperand.cpp
+ * @author Phuong Diep Thanh (diepthanhphuong0407@gmail.com)
+ * @version 2.0
+ * @date 2022-07-03
+ * @copyright Copyright (c) 2022
+ */
+#pragma once
+#ifndef IS_OPERAND_CPP
+#define IS_OPERAND_CPP
+
+#include <iostream>
+
+namespace ExpressionManagement
+{
+    /**
+     * @brief IsOperand checks whether input expression is operand or not
+     * @brief Operands are variables, numbers, constants in version 2.0
+     * @param Checking is input we want to check
+     * @return true
+     */
+    bool IsOperand(char Checking)
+    {
+        if (Checking >= 'a' && Checking <= 'z')
+            return true;
+        else if (Checking >= 'A' && Checking <= 'Z')
+            return true;
+        else if (Checking >= '0' && Checking <= '9')
+            return true;
+        else if (Checking == '.')
+            return true;
+        else
+            return false;
+    }
+}
+#endif
+
+// using namespace std;
+// using namespace ExpressionManagement;
+// int main()
+// {
+//     if (IsOperand('1') == true)
+//         cout << "true";
+// }
