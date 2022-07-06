365f742b9fd860345115657a16dc45119a55220f
Phuong0407 <diepthanhphuong0407@gmail.com>
2022-07-06 21:32:25 +0700

    Remove one file

--- SymbolicComputation/Expression/ExpressionObject/ExpressionCustomTokenObject.h
+++ SymbolicComputation/Expression/ExpressionObject/ExpressionCustomTokenObject.h
@@ -10,35 +10,71 @@
 #ifndef EXPRESSION_CUSTOM_TOKEN_OBJECT_H
 #define EXPRESSION_CUSTOM_TOKEN_OBJECT_H
 
-#include "ExpressionStringObject.h"
+#include "../BuildingBlock/ExpressionPlainBuildingBlock.h"
 #include "../ProcessingMethod/ProcessingMainMethodAPI.cpp"
 
 namespace ExpressionManagement
 {
-    class ExpressionCustomTokenObject : public ExpressionStringObject
+    class ExpressionCustomTokenObject
     {
     private:
+        std::string ExpressionInputInitial;
+        std::string ExpressionInputNoBlank;
         CustomTokenUnit ExpressionCustomTokenInfix;
         CustomTokenUnit ExpressionCustomTokenPostfix;
 
     protected:
+        void RemoveUnaryOperator();
+        std::string OutputExpressionInitial();
+        std::string OutputExpressionNoBlank();
         void ExpressionCustomTokenInputAPI(std::string);
         CustomTokenUnit OutputExpressionCustomTokenInfix();
         CustomTokenUnit OutputExpressionCustomTokenPostfix();
     };
     typedef class ExpressionCustomTokenObject ExpressionCustomTokenObject;
 
+    void ExpressionCustomTokenObject::RemoveUnaryOperator()
+    {
+        if (this->ExpressionInputNoBlank[0] == '+' || this->ExpressionInputNoBlank[0] == '-')
+        {
+            std::string TempStringInsert = "";
+            TempStringInsert += '0';
+            TempStringInsert += this->ExpressionInputNoBlank;
+            this->ExpressionInputNoBlank.assign(TempStringInsert);
+        }
+        for (unsigned int i = 0; i < this->ExpressionInputNoBlank.length(); ++i)
+            if (this->ExpressionInputNoBlank[i] == '(' && (this->ExpressionInputNoBlank[i + 1] == '+' || this->ExpressionInputNoBlank[i + 1] == '-'))
+                this->ExpressionInputNoBlank.insert(i + 1, "0");
+    }
+
     void ExpressionCustomTokenObject::ExpressionCustomTokenInputAPI(std::string ExpressionStringInput)
     {
-        this->ExpressionStringObjectInputAPI(ExpressionStringInput);
+        this->ExpressionInputInitial = ExpressionStringInput;
+        do
+        {
+            ExpressionStringInput.erase(remove(ExpressionStringInput.begin(), ExpressionStringInput.end(), ' '), ExpressionStringInput.end());
+        } while (ExpressionStringInput.find(' ') == ExpressionStringInput.length());
+        this->ExpressionInputNoBlank = ExpressionStringInput;
+        this->RemoveUnaryOperator();
         this->ExpressionCustomTokenInfix = DecompositionToInfixCustomToken(this->OutputExpressionNoBlank());
         this->ExpressionCustomTokenPostfix = InfixToPostfixConversion(this->OutputExpressionCustomTokenInfix());
+    }
+
+    std::string ExpressionCustomTokenObject::OutputExpressionInitial()
+    {
+        return this->ExpressionInputInitial;
+    }
+
+    std::string ExpressionCustomTokenObject::OutputExpressionNoBlank()
+    {
+        return this->ExpressionInputNoBlank;
     }
 
     CustomTokenUnit ExpressionCustomTokenObject::OutputExpressionCustomTokenInfix()
     {
         return this->ExpressionCustomTokenInfix;
     }
+
     CustomTokenUnit ExpressionCustomTokenObject::OutputExpressionCustomTokenPostfix()
     {
         return this->ExpressionCustomTokenPostfix;
